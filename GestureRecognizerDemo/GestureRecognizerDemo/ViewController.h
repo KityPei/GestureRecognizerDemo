ba433ccbe6c7b1d4e5e5c9719a84ae604533a41c
Kity_Pei <Kity_Pei@KityPeideMacBook-Pro.local>
2016-06-01 23:54:25 +0800

    创建空项目

---
+++ GestureRecognizerDemo/GestureRecognizerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureRecognizerDemo
+//
+//  Created by Kity_Pei on 16/6/1.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GestureRecognizerDemo/GestureRecognizerDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  GestureRecognizerDemo
+//
+//  Created by Kity_Pei on 16/6/1.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
